44235ab812ab87d1270d0910775c1c0b3d22a1e3
NTruong <quang072000@gmail.com>
2025-12-30 12:51:59 -0600

    LeetCode 91 with my comments

--- 91.DecodeWays.py
+++ 91.DecodeWays.py
@@ -1,29 +1,28 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        # Not 0 or not string,check 1st character
         if not s or s[0] == "0":
             return 0
-        
-        #dp[i-2], dp[i-1]
-        two_back = 1 #dp[0]
-        one_back = 1 #dp[1]
+        # Define dp[i-1] (one_back) and dp[i-2] (two back)
+        one_back = 1 #first number is not 0 so always at least 1 , dp[1]=1
+        two_back = 1 #default, dp[0]=1, "1" is valid → one way to decode
 
-
+        # Loop from 2nd character until the end of s
         for i in range(1,len(s)):
+        # current is 0 every loop and adds one_back if one check is good, current += if two_back is good
             current = 0
-            
-            #One digit decode:
-            if s[i] != "0":
+
+            if s[i]!="0":
                 current += one_back
 
-        
-            #Two-digit decode:
-            two_digit = int(s[i-1:i+1])
-            if 10 <= two_digit <= 26:
+            two_digits=s[i-1:i+1]
+            if 10<=int(two_digits)<=26:
                 current += two_back
+
             
-            #Shift window
-            two_back = one_back
-            one_back = current
+        # move window: two_back = one_back, one_back = current 
+            two_back = one_back #dp[i-1]
+            one_back = current # dp[i]
 
+        # return one_back
         return one_back
-
